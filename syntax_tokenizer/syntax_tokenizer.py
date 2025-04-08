d961f168da0916866f7d52b613b4dd29ce5a78f5
Aman Gupta <gupta.aman@gmail.com>
2025-04-07 18:33:12 -0700

    Adding an encoding api that implements some of the args that HF tokenizers support. This allows our training scripts to function with no changes.

--- syntax_tokenizer/syntax_tokenizer.py
+++ syntax_tokenizer/syntax_tokenizer.py
@@ -1,101 +1,208 @@
 from collections import defaultdict
-from datasets import load_dataset
+from dataclasses import dataclass
 from tqdm import tqdm
 
 import bisect
 import math
 import numpy as np
 import pickle
+import torch
 
 import spacy
 from spacy_syllables import SpacySyllables
 
+@dataclass
+class SyntaxTokenizerData:
+    spacy_model: str
+    lang: str
+
+    eos_token: str
+    eos_token_id: int
+
+    pad_token: str
+    pad_token_id: int
+
+    num_special_tokens: int
+
+    pos_to_int: dict[str, int]
+
+    idf: dict[str, float]
+    bin_edges: np.ndarray[np.float64]
+
+    max_syllables: int
+
 
 class SyntaxTokenizer:
     def __init__(self, spacy_model="en_core_web_sm", lang="en_US"):
+        self.spacy_model = spacy_model
+        self.lang = lang
+
         self.nlp = spacy.load(spacy_model, exclude=['ner'])
         self.nlp.add_pipe("syllables", after="tagger", config={"lang": lang})
-        #print(self.nlp.pipeline)
 
 
     def train(self, texts):
-        self.pos_to_int = {}
+        pos_to_int = {}
         for i, label in enumerate(self.nlp.get_pipe("tagger").labels):
             #print(label, " -- ", spacy.explain(label))
-            self.pos_to_int[label] = i + 1
+            pos_to_int[label] = i + 1
 
         #print(self.pos_to_int)
 
-        self.max_syllables = 0
-        self.idf = defaultdict(int)
+        max_syllables = 0
+        idf = defaultdict(int)
 
         for doc in tqdm(self.nlp.pipe(texts, n_process=16)):
             unique_tokens = set()
             for token in doc:
-                self.max_syllables = max(self.max_syllables, token._.syllables_count or 0)
+                max_syllables = max(max_syllables, token._.syllables_count or 0)
                 unique_tokens.add(token.lemma)
                 #print(f"Text: {token.text}; syllables: { token._.syllables}, {token._.syllables_count}")
 
             for token in unique_tokens:
-                self.idf[token] += 1
+                idf[token] += 1
 
 #        print(self.idf)
 
         n = len(texts)
-        self.idf = {k: math.log(n/v) for k, v in self.idf.items()}
-        idf_vals = np.array(list(self.idf.values()), dtype=np.float32)
+        idf = {k: math.log(n/v) for k, v in idf.items()}
+        idf_vals = np.array(list(idf.values()), dtype=np.float32)
         # Discretize idf values using dynamic bin edges
-        self.bin_edges = np.histogram_bin_edges(idf_vals, bins='sturges')
+        bin_edges = np.histogram_bin_edges(idf_vals, bins='sturges')
 
         # to allow for None, and some uniquely complex words not in training set
-        self.max_syllables = self.max_syllables + 2
+        max_syllables = max_syllables + 2
+        #print(max_syllables)
 
-        #print(self.max_syllables)
+        self.data = SyntaxTokenizerData(
+            spacy_model=self.spacy_model,
+            lang=self.lang,
+            eos_token="</s>",
+            eos_token_id=0,
+            pad_token="</s>",
+            pad_token_id=0,
+            num_special_tokens=1,
+            pos_to_int=pos_to_int,
+            idf=idf,
+            bin_edges=bin_edges,
+            max_syllables=max_syllables,
+        )
 
     def save(self, path):
-        pickle.dump(self, open(path, "wb"))
+        with open(path, "wb") as f:
+            pickle.dump(self.data, f)
 
     @staticmethod
     def load(path):
-        return pickle.load(open(path, "rb"))
+        with open(path, "rb") as f:
+            tokenizer_data = pickle.load(f)
+
+        tokenizer = SyntaxTokenizer()
+        tokenizer.data = tokenizer_data
+
+        return tokenizer
 
     def vocab_size(self):
-        return (self.max_syllables +
-                (self.max_syllables+1) * len(self.bin_edges) +
-                (self.max_syllables + 1) * (len(self.bin_edges) + 1) * len(self.pos_to_int)
+        return self.data.num_special_tokens + (self.data.max_syllables +
+                (self.data.max_syllables + 1) * len(self.data.bin_edges) +
+                (self.data.max_syllables + 1) * (len(self.data.bin_edges) + 1) * len(self.data.pos_to_int)
                )
 
-    def encode(self, doc, return_str=False):
+    # return value --> { "input_ids": <>, "attention_mask": <> }
+    def __call__(self,
+                 texts,
+                 max_length=-1,
+                 padding="max_length",
+                 truncation=False,
+                 return_overflowing_tokens=False,
+                 stride=0,
+                 return_tensors="py"):
+        if not isinstance(texts, list):
+            texts = [texts]
+
+        input_ids = []
+        for text in texts:
+            token_ids = self.encode(text, max_length, truncation, return_overflowing_tokens, stride)
+            input_ids.extend(token_ids)
+
+        if return_tensors != "py" and (padding == "do_not_pad" or not padding):
+            padding = "longest"
+
+        pad_to_length = 0
+        match padding:
+            case True | "longest":
+                pad_to_length = max(len(id_seq) for id_seq in input_ids)
+            case "max_length":
+                pad_to_length = max_length
+
+        attention_masks = []
+        for seq in input_ids:
+            k = len(seq)
+            pad_tokens = max(pad_to_length - k, 0)
+            seq.extend([self.data.pad_token_id] * pad_tokens)
+            attention_masks.append([1] * k + [0] * pad_tokens)
+
+        match return_tensors:
+            case "pt":
+                dtype = torch.int16 if self.vocab_size() < 16384 else torch.int32
+                input_ids = torch.tensor(input_ids, dtype=dtype)
+                attention_masks = torch.tensor(attention_masks, dtype=dtype)
+            case "np":
+                dtype = np.int16 if self.vocab_size() < 16384 else np.int32
+                input_ids = np.array(input_ids, dtype=dtype)
+                attention_masks = np.array(attention_masks, dtype=dtype)
+
+        return {"input_ids": input_ids, "attention_mask": attention_masks}
+
+
+    # return value --> [[token seq]]
+    def encode(self,
+               text,
+               max_length=-1,
+               truncation=False,
+               return_overflowing_tokens=False,
+               stride=0):
+
         encoding = []
-        for token in self.nlp(doc):
+        for token in self.nlp(text):
             num_syllables = token._.syllables_count
             if not num_syllables:
                 num_syllables = 0
-            num_syllables = min(num_syllables, self.max_syllables)
+            num_syllables = min(num_syllables, self.data.max_syllables)
 
-            idf = self.idf.get(token.text, 0.001)
-            idf_bin = bisect.bisect_left(self.bin_edges, idf)
+            idf = self.data.idf.get(token.text, 0.001)
+            idf_bin = bisect.bisect_left(self.data.bin_edges, idf)
 
-            token_val = (num_syllables +
-                         (self.max_syllables+1) * idf_bin +
-                         (self.max_syllables+1) * (len(self.bin_edges)+1) *  self.pos_to_int[token.tag_]
+            token_val = self.data.num_special_tokens + (num_syllables +
+                         (self.data.max_syllables+1) * idf_bin +
+                         (self.data.max_syllables+1) * (len(self.data.bin_edges)+1) *  self.data.pos_to_int[token.tag_]
                         )
 
-            if return_str:
-                encoding.append((token.text, token_val))
-            else:
-                encoding.append(token_val)
 
-        return encoding
+            encoding.append(token_val)
+        encoding.append(self.data.eos_token_id)
+
+        encodings = [encoding]
+        if max_length > 0:
+            if return_overflowing_tokens:
+                assert stride < max_length
+                n = len(encoding)
+                encodings = [encoding[i:i+max_length] for i in range(0, n-stride, max_length-stride)]
+            elif truncation:
+                del encoding[max_length:]
+
+        return encodings
+
+
+    def decode(self, encoding):
+        pass
+
+    def batch_decode(self, encoding):
+        pass
+
 
 def main():
-    # load docs dataset
-    docs_ds = load_dataset('amang1802/synthetic_data_topic_conditioned_L3.3_70B')['train']
-    docs = docs_ds['text']
-
-    # run train
-    tokenizer = SyntaxTokenizer()
-    tokenizer.train(docs)
+    tokenizer = SyntaxTokenizer.load("tokenizer.data")
 
     print(f"Vocab size: {tokenizer.vocab_size()}")
 
@@ -105,15 +212,12 @@ def main():
     text4 = "A magnitude-7.7 earthquake leaves more than 4,300 people dead in Myanmar and Thailand."
 
     # encode some sample texts
-    print(tokenizer.encode(text1, return_str=True))
-    print(tokenizer.encode(text2, return_str=True))
-    print(tokenizer.encode(text3, return_str=True))
-    print(tokenizer.encode(text4, return_str=True))
+    print(tokenizer.encode(text1))
+    print(tokenizer.encode(text2))
+    print(tokenizer.encode(text3))
+    print(tokenizer.encode(text4))
 
-    tokenizer.save("tokenizer.data")
-    tok2 = SyntaxTokenizer.load("tokenizer.data")
-
-    print(tok2.encode(text1, return_str=True))
+    print(tokenizer([text1, text2, text3, text4], stride=3, padding=False, return_tensors="pt"))
 
 
 if __name__ == '__main__':
